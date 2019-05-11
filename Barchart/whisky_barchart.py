1feafe472caac0cece1c5c8e0893d677609ff005
Jacques Sham <jacquessham2015@gmail.com>
2019-05-11 16:09:05 -0700

    Change bar chart

--- Barchart/whisky_barchart.py
+++ Barchart/whisky_barchart.py
@@ -6,13 +6,13 @@ from plotly.offline import *
 # To initiate ploty to run offline
 init_notebook_mode(connected=True)
 
-price = pd.read_csv('../Data/price.csv')
+price = pd.read_csv('../Data/whisky.csv')
 
-region_size = price.groupby('region').size()
+region_size = price.groupby('Region').size()
 region_size = pd.DataFrame(region_size,columns=['count'])
 region_size.reset_index(level=0, inplace=True)
 region_size = region_size.sort_values(by='count')
-region = region_size['region'].tolist()
+region = region_size['Region'].tolist()
 count = region_size['count'].tolist()
 count_text = [str(num) for num in count]
 
@@ -21,7 +21,7 @@ bar = [go.Bar(x=region,
 	           textposition='auto')]
 
 layout = go.Layout(
-    title='Choice of Scotch Whisky by Region',
+    title='Distillery Count of Scotch Whisky by Region',
     xaxis=dict(title='Region of Scotland'),
     yaxis=dict(title='Count'))
 fig = go.Figure(data=bar, layout=layout)
